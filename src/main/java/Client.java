4e68ba65a6842aec89cce5c7c3bf09f464f3ae91
Vladimir Troshin <vtroshin@rvision.pro>
2022-06-08 20:41:17 +0300

    Add one more try catch block

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -22,6 +22,8 @@ public class Client {
 
                 System.out.println(bufferedReader.readLine());
             }
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
